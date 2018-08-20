07a38b3ce921468b9a84f8f5808094aef5ee0f71
junngo <myeongjun.ko@gmail.com>
2018-08-21 00:40:01 +0900

    [Django] Liking an image

--- awesomeplace/images/views.py
+++ awesomeplace/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 class Feed(APIView):
@@ -22,3 +23,36 @@ class Feed(APIView):
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+
+class LikeImages(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        user = request.user
+
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image                 
+            )
+            preexisiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+       
+            new_like = models.Like.objects.create(
+                creator = user,
+                image = found_image 
+            )
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
