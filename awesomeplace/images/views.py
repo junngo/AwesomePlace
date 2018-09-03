b257879d25567295a2fdbbe3b77087e7625bb922
junngo <myeongjun.ko@gmail.com>
2018-09-03 23:49:58 +0900

    [Django] Getting the image likes

--- awesomeplace/images/views.py
+++ awesomeplace/images/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from awesomeplace.users import models as user_models
+from awesomeplace.users import serializers as user_serializers
 from awesomeplace.notifications import views as notification_views
 
 
@@ -33,6 +35,16 @@ class Feed(APIView):
 
 
 class LikeImages(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        likes = models.Like.objects.filter(image__id=image_id)
+        like_creators_ids = likes.values('creator_id')
+
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
