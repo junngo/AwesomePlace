eb51d2148e377b473937d1860ac67d462b40a975
junngo <myeongjun.ko@gmail.com>
2018-10-08 21:03:55 +0900

    [React] Syncing react router with redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import store from "redux/configureStore";
+import { ConnectedRouter } from "react-router-redux";
+import store, { history } from "redux/configureStore";
 import 'index.css';
 import App from 'App';
 
@@ -10,7 +11,9 @@ store.dispatch({type: "good" });
 
 ReactDOM.render(
     <Provider store ={store}>
+    <ConnectedRouter history={history}>
         <App />
+    </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
